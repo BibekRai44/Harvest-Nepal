7faa3fb81fa51dab7624bc0de297c35e68cec9a1
Bibekkun <bibekrai655@gmail.com>
2023-06-10 17:48:56 +0545

    added route for prediction

--- app.py
+++ app.py
@@ -26,5 +26,30 @@ def crop():
 def fertilizer():
     return render_template("fertilizer.html")
 
+@app.route("/crop-prediction", methods=["POST"])
+def crop_prediction():
+    if request.method == "POST":
+        # Get the input values from the form
+        nitrogen = int(request.form["nitrogen"])
+        phosphorous = int(request.form["phosphorous"])
+        potassium = int(request.form["potassium"])
+        temperature = int(request.form["temperature"])
+        humidity = int(request.form["humidity"])
+        rainfall = int(request.form["rainfall"])
+        ph_value = int(request.form["ph_value"])
+
+        # Prepare the input data as a numpy array
+        input_data = np.array([[nitrogen, phosphorous, potassium, temperature, humidity, rainfall, ph_value]])
+
+        # Perform crop prediction using the loaded model
+        crop_prediction = model.predict(input_data)
+        crop_name = crop_prediction[0]
+
+        # Return the result as a JSON response
+        return jsonify({"prediction_text": crop_name})
+
+    # Handle GET request if required
+    return render_template("crop.html")
+
 if __name__ == "__main__":
     app.run()
